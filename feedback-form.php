88af3f6204e1b0502a30fe4faf0cd5f9c2a936a5
portiaocran <portiaocran3@gmail.com>
2024-10-09 00:33:01 -0400

    - updated email form

--- feedback-form.php
+++ feedback-form.php
@@ -1,19 +1,20 @@
 <?php
+   $to = "portiaocran3@gmail.com";
+   $subject = "This is subject";
 
-    $myAwardSpaceEmail = "portiaocran3@gmail.com";
-    $myPersonalEmail = "portiaocran3@gmail.com";
-    
-    if(isset($_POST['submit'])) {
-        $subject = $_POST['subject'];
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
-        $headers = "From: Contact Form <" . $myAwardSpaceEmail . ">" . "\r\n";
-        $headers .= "Reply-To: " . $name . " <" . $email .">" . "\r\n";
-        
-        echo 'Your message has been sent successfully!';
-        mail($myPersonalEmail, $subject, $message, $headers);
-    } else {
-        echo 'An error has occurred!' + $name;
-    }
+   $message = "<b>This is HTML message.</b>";
+   $message .= "<h1>This is headline.</h1>";
+
+   $header = "From:abc@somedomain.com \r\n";
+   $header .= "Cc:afgh@somedomain.com \r\n";
+   $header .= "MIME-Version: 1.0\r\n";
+   $header .= "Content-type: text/html\r\n";
+
+   $retval = mail ($to,$subject,$message,$header);
+
+   if( $retval == true ) {
+      echo "Message sent successfully...";
+   }else {
+      echo "Message could not be sent...";
+   }
 ?>
