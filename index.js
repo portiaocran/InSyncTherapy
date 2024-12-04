382b3ee295844010a271c1f08265fc45d49cf861
portiaocran <portiaocran3@gmail.com>
2024-12-04 00:52:02 -0500

    linked buttons on first page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const exampleModal = document.getElementById('exampleModal')
+const exampleModal = document.getElementById('open-form')
 if (exampleModal) {
   exampleModal.addEventListener('show.bs.modal', event => {
     // Button that triggered the modal
@@ -11,6 +11,64 @@ if (exampleModal) {
     // Update the modal's content.
     const modalTitle = exampleModal.querySelector('.modal-title')
     const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+
+const customBtn = document.getElementById('custom-btn')
+const customBtn2 = document.getElementById('custom-btn-2')
+
+if (customBtn) {
+  customBtn.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = customBtn.querySelector('.modal-title')
+    const modalBodyInput = customBtn.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+
+if (customBtn2) {
+  customBtn2.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = customBtn.querySelector('.modal-title')
+    const modalBodyInput = customBtn.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+
+const boxBtn = document.getElementById('custom-btn-boxes')
+if (boxBtn) {
+  boxBtn.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = customBtn.querySelector('.modal-title')
+    const modalBodyInput = customBtn.querySelector('.modal-body input')
 
     modalTitle.textContent = `New message to ${recipient}`
     modalBodyInput.value = recipient
